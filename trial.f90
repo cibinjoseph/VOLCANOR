bfdc050398c7f994273a28988ceb58131010a84e
Cibin <cibinjoseph92@gmail.com>
2018-01-14 03:01:31 +0530

    Trial for inverse checking

--- trial.f90
+++ trial.f90
@@ -1,22 +1,13 @@
- PROGRAM TRIAL
- 
- INTEGER NTHREADS, TID, OMP_GET_NUM_THREADS, OMP_GET_THREAD_NUM
- 
-! Fork a team of threads giving them their own copies of variables
-!$OMP PARALLEL PRIVATE(NTHREADS, TID)
+program trial
+  use mymathlib
+  implicit none 
+  real(dp), dimension(2,2) :: A, A_inv
+
+  A(1,:)=(/1._dp,2._dp/)
+  A(2,:)=(/3._dp,4._dp/)
+
+  A_inv=inv(A)
+  call print_mat(inv(A))
 
 
-! Obtain thread number
- TID = OMP_GET_THREAD_NUM()
- PRINT *, 'Hello World from thread = ', TID
-
-! Only master thread does this
- IF (TID .EQ. 0) THEN
-  NTHREADS = OMP_GET_NUM_THREADS()
- PRINT *, 'Number of threads = ', NTHREADS
- END IF
-
-! All threads join master thread and disband
-!$OMP END PARALLEL
-
- END
+end program trial
