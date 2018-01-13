52ef11614fd0355cf161b62e694870865d0856c2
Cibin <cibinjoseph92@gmail.com>
2018-01-14 03:37:10 +0530

    For verifying inv()

--- trial.f90
+++ trial.f90
@@ -1,12 +1,12 @@
 program trial
   use mymathlib
   implicit none 
-  real(dp), dimension(2,2) :: A, A_inv
+  real(dp), dimension(3,3) :: A
 
-  A(1,:)=(/1._dp,2._dp/)
-  A(2,:)=(/3._dp,4._dp/)
+  A(1,:)=(/1._dp,2._dp,1._dp/)
+  A(2,:)=(/3._dp,4._dp,6._dp/)
+  A(3,:)=(/5._dp,1._dp,3._dp/)
 
-  A_inv=inv(A)
   call print_mat(inv(A))
 
 
