9a4634295ae672b5a50c99b77a8bf14adaea1071
Cibin <cibinjoseph92@gmail.com>
2018-01-16 14:12:48 +0530

    Added drag2file() and drag in addheaders()

--- postproc.f90
+++ postproc.f90
@@ -143,19 +143,37 @@ contains
     type(wingpanel_class), intent(in), dimension(:,:) :: wing_array
     character(*), intent(in) :: filename
     real(dp), intent(in), dimension(:) :: extra_params ![1]time [2]chord [3]span [4]speed
-    real(dp), dimension(3) :: Force
+    real(dp) :: lift
     integer :: i,j
     Force=0._dp
 
     do j=1,size(wing_array,2)
       do i=1,size(wing_array,1)
-        Force=Force+wing_array(i,j)%dForce
+        lift=lift+wing_array(i,j)%dForce(3)
       enddo
     enddo
     open(unit=10,file=filename,position='append')
-    write(10,*) extra_params(1),Force(3)!,Force(3)/(0.5_dp*1.2_dp*extra_params(4)**2._dp*extra_params(2)*extra_params(3))
+    write(10,*) extra_params(1),lift!,lift/(0.5_dp*1.2_dp*extra_params(4)**2._dp*extra_params(2)*extra_params(3))
     close(10)
   end subroutine lift2file
+
+  subroutine drag2file(wing_array,filename,extra_params)
+    type(wingpanel_class), intent(in), dimension(:,:) :: wing_array
+    character(*), intent(in) :: filename
+    real(dp), intent(in), dimension(:) :: extra_params ![1]time [2]chord [3]span [4]speed
+    real(dp) :: drag
+    integer :: i,j
+    drag=0._dp
+
+    do j=1,size(wing_array,2)
+      do i=1,size(wing_array,1)
+        drag=drag+wing_array(i,j)%dForce(1)
+      enddo
+    enddo
+    open(unit=10,file=filename,position='append')
+    write(10,*) extra_params(1),drag!,drag/(0.5_dp*1.2_dp*extra_params(4)**2._dp*extra_params(2)*extra_params(3))
+    close(10)
+  end subroutine drag2file
 
   subroutine addheaders()
     integer :: stat
@@ -168,6 +186,16 @@ contains
     if (stat .ne. 0) error stop 'ERROR: Unable to perform header write operation (Step 3)'
     call execute_command_line('rm dummyheader',exitstat=stat)
     if (stat .ne. 0) error stop 'ERROR: Unable to perform header write operation (Step 4)'
+
+    ! For drag.curve
+    call execute_command_line("echo '# Drag' > dummyheader",exitstat=stat)
+    if (stat .ne. 0) error stop 'ERROR: Unable to perform header write operation (Step 5)'
+    call execute_command_line("cat dummyheader Results/drag.curve > dummyfile ",exitstat=stat)
+    if (stat .ne. 0) error stop 'ERROR: Unable to perform header write operation (Step 6)'
+    call execute_command_line("mv dummyfile Results/drag.curve",exitstat=stat)
+    if (stat .ne. 0) error stop 'ERROR: Unable to perform header write operation (Step 7)'
+    call execute_command_line('rm dummyheader',exitstat=stat)
+    if (stat .ne. 0) error stop 'ERROR: Unable to perform header write operation (Step 8)'
   end subroutine addheaders
 
 end module postproc
