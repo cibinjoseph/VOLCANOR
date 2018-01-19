cf0e072ac653097f1e6c84ae0876d3479ccfe531
Cibin <cibinjoseph92@gmail.com>
2018-01-19 18:25:08 +0530

    Removed test drag1()

--- library.f90
+++ library.f90
@@ -543,6 +543,7 @@ contains
     gam_prev=reshape(gamvec_prev,(/rows,cols/))
     do j=1,cols
       do i=2,rows
+        print*,vind_panelgeo(wake_array,wg(i,j)%cp)-(wg(i,j)%velCP-wg(i,j)%velCPm)
         !vel_drag=dot_product((vind_panelgeo(wake_array,wg(i,j)%cp))+vind_chordvortex(wg,wg(i,j)%cp),&
         !  (/0._dp,0._dp,1._dp/))
         vel_drag=dot_product((wg(i,j)%velCP-wg(i,j)%velCPm)+vind_chordvortex(wg,wg(i,j)%CP),&
@@ -573,55 +574,4 @@ contains
       enddo
     enddo
   end function calcdrag
-
-  function calcdrag1(wg,gamvec_prev,dt)
-    type(wingpanel_class), intent(inout), dimension(:,:) :: wg !short form for wing_array
-    real(dp), intent(in), dimension(:) :: gamvec_prev
-    real(dp) :: calcdrag, calcdrag1
-    real(dp), intent(in) :: dt
-    real(dp) :: vel_drag
-    real(dp) :: drag1, drag2
-    real(dp), dimension(size(wg,1),size(wg,2)) :: gam_prev
-    integer :: i,j,rows,cols
-    ! Inherent assumption that panels have subdivisions along chord and not inclined to it
-    ! while calculating tangent vector
-    ! LE and left sides used for calculating tangent vectors
-
-    ! *** !! PREDICTS DRAG1 INCORRECTLY !! ***
-    ! *** !! PREDICTS DRAG1 INCORRECTLY !! ***
-    ! *** !! PREDICTS DRAG1 INCORRECTLY !! ***
-
-    rows=size(wg,1)
-    cols=size(wg,2)
-
-    gam_prev=reshape(gamvec_prev,(/rows,cols/))
-    do j=1,cols
-      do i=2,rows
-        vel_drag=dot_product((wg(i,j)%velCP-wg(i,j)%velCPm)+vind_chordvortex(wg,wg(i,j)%CP),&
-          matmul(wg(i,j)%orthproj(),wg(i,j)%ncap))
-        drag2=(wg(i,j)%vr%gam-gam_prev(i,j))*wg(i,j)%panel_area*sin(wg(i,j)%alpha)/dt
-        drag1=-vel_drag*(wg(i,j)%vr%gam-wg(i-1,j)%vr%gam)*norm2(wg(i,j)%pc(:,4)-wg(i,j)%pc(:,1))
-        wg(i,j)%dDrag=drag1-drag2
-      enddo
-    enddo
-
-    ! i=1
-    do j=2,cols
-      vel_drag=dot_product((wg(1,j)%velCP-wg(1,j)%velCPm)+vind_chordvortex(wg,wg(1,j)%CP),&
-        matmul(wg(1,j)%orthproj(),wg(1,j)%ncap))
-      drag2=(wg(1,j)%vr%gam-gam_prev(1,j))*wg(1,j)%panel_area*sin(wg(1,j)%alpha)/dt
-      drag1=-vel_drag*(wg(1,j)%vr%gam)*norm2(wg(1,j)%pc(:,4)-wg(1,j)%pc(:,1))
-      wg(1,j)%dDrag=drag1-drag2
-    enddo
-
-    wg%dDrag=density*wg%dDrag
-
-    calcdrag=0._dp
-    do j=1,cols
-      do i=1,rows
-        calcdrag=calcdrag+wg(i,j)%dDrag
-      enddo
-    enddo
-  end function calcdrag1
-
 end module library
