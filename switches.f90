4c3480e4f293ab9482c0e315405535ec1c590e92
Cibin <cibinjoseph92@gmail.com>
2018-02-12 17:56:56 +0530

    testcase

--- switches.f90
+++ switches.f90
@@ -2,14 +2,14 @@
 ! Vortex model: [1]Ideal Vortex [2]Rankine Vortex
 integer, parameter :: model_switch=2
 
-! Span discretization : [1]linear [2]cosine[3]halfsine
+! Span discretization : [1]linear [2]cosine [3]halfsine
 integer, parameter :: span_spacing_switch=2
 
 ! Wake tip dissipation: [0]Off [1]On
 integer, parameter :: tip_diss_switch=0
 
 ! Slow start to avoid large starting vortex: [0]Off [1]linear [2]tanh [3]extended tanh
-integer, parameter :: slowstart_switch=1
+integer, parameter :: slowstart_switch=0
 integer, parameter :: slowstart_nt=20
 
 ! Wake strain: [0]Off [1]On
@@ -19,7 +19,7 @@ integer, parameter :: wakestrain_switch=0
 integer, parameter :: wakeplot_switch=2
 
 ! Wake convection using Predictor-Corrector approach [0]Off [1]On
-integer, parameter :: PCwake_switch=1
+integer, parameter :: PCwake_switch=0
 
 ! Ignore Wake wake interaction [0]Off [1..]timesteps
-integer, parameter :: wake_ignore_nt=100
+integer, parameter :: wake_ignore_nt=0
