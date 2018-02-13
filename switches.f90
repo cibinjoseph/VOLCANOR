aae479228dbe41711e40c1cb190c4f917bb017c2
Cibin <cibinjoseph92@gmail.com>
2018-02-13 18:25:01 +0530

    testcase

--- switches.f90
+++ switches.f90
@@ -19,7 +19,7 @@ integer, parameter :: wakestrain_switch=0
 integer, parameter :: wakeplot_switch=2
 
 ! Wake convection using Predictor-Corrector approach [0]Off [1]On
-integer, parameter :: PCwake_switch=0
+integer, parameter :: PCwake_switch=2
 
 ! Ignore Wake wake interaction [0]Off [1..]timesteps
-integer, parameter :: wake_ignore_nt=250
+integer, parameter :: wake_ignore_nt=20
