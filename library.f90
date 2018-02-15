e0b9721cf20bd1d8849286f537784ea79889571a
Cibin <cibinjoseph92@gmail.com>
2018-02-15 17:36:27 +0530

    testcase

--- library.f90
+++ library.f90
@@ -5,7 +5,7 @@ module library
   implicit none
 
   ! Input parameters
-  integer, parameter  :: nt = 100
+  integer, parameter  :: nt = 300
   integer, parameter  :: ns = 13
   integer, parameter  :: nc = 5
 
--- switches.f90
+++ switches.f90
@@ -22,4 +22,4 @@ integer, parameter :: wakeplot_switch=2
 integer, parameter :: PCwake_switch=2
 
 ! Ignore Wake wake interaction [0]Off [1..]timesteps
-integer, parameter :: wake_ignore_nt=0
+integer, parameter :: wake_ignore_nt=144
