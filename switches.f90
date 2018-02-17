0212a5589fd1d01ed1238d8451f3f1fa23fdea31
Cibin <cibinjoseph92@gmail.com>
2018-02-17 14:48:09 +0530

    testcase

--- switches.f90
+++ switches.f90
@@ -1,6 +1,6 @@
 ! This file contains the switches that control certain features in the code
 ! Vortex model: [1]Ideal Vortex [2]Rankine Vortex
-integer, parameter :: model_switch=1
+integer, parameter :: model_switch=2
 
 ! Span discretization : [1]linear [2]cosine [3]halfsine
 integer, parameter :: span_spacing_switch=2
@@ -9,7 +9,7 @@ integer, parameter :: span_spacing_switch=2
 integer, parameter :: tip_diss_switch=0
 
 ! Slow start to avoid large starting vortex: [0]Off [1]linear [2]tanh [3]extended tanh
-integer, parameter :: slowstart_switch=0
+integer, parameter :: slowstart_switch=1
 integer, parameter :: slowstart_nt=144
 
 ! Wake strain: [0]Off [1]On
