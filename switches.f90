2ff4958adb689f41abe34cd4b8a5a1baf3d12f4a
cibinjoseph <cibinjoseph92@gmail.com>
2018-02-27 16:01:43 +0530

    testcase

--- switches.f90
+++ switches.f90
@@ -9,8 +9,8 @@ integer, parameter :: span_spacing_switch=2
 integer, parameter :: tip_diss_switch=0
 
 ! Slow start to avoid large starting vortex: [0]Off [1]linear [2]tanh [3]extended tanh
-integer, parameter :: slowstart_switch=1
-integer, parameter :: slowstart_nt=288
+integer, parameter :: slowstart_switch=0
+integer, parameter :: slowstart_nt=144
 
 ! Wake strain: [0]Off [1]On
 integer, parameter :: wakestrain_switch=0
@@ -19,7 +19,7 @@ integer, parameter :: wakestrain_switch=0
 integer, parameter :: wakeplot_switch=2
 
 ! Wake convection finite-difference scheme [0]FT1 [1]PC2 [2]AB2 [3]AB4
-integer, parameter :: FDscheme_switch=2
+integer, parameter :: FDscheme_switch=1
 
 ! Ignore Wake wake interaction [0]Off [1..]timesteps
 integer, parameter :: wake_ignore_nt=0
