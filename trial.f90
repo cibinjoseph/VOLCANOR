294e8d04a591d227c3a854d90a0bd941acec5a62
cibinjoseph <cibinjoseph92@gmail.com>
2018-03-05 02:40:59 +0530

    trial

--- trial.f90
+++ trial.f90
@@ -1,13 +1,27 @@
 program trial
   use mymathlib
   implicit none 
-  real(dp), dimension(3,3) :: A
+  real(dp), dimension(3,2,2) :: A
+  real(dp), dimension(2,2) :: a1,a2,a3
 
-  A(1,:)=(/1._dp,2._dp,1._dp/)
-  A(2,:)=(/3._dp,4._dp,6._dp/)
-  A(3,:)=(/5._dp,1._dp,3._dp/)
+  a1(1,:)=(/1._dp,1._dp/)
+  a1(2,:)=(/1._dp,1._dp/)
+  
+  a2(1,:)=(/2._dp,2._dp/)
+  a2(2,:)=(/2._dp,2._dp/)
+  
+  a3(1,:)=(/3._dp,3._dp/)
+  a3(2,:)=(/3._dp,3._dp/)
 
+  A(1,:,:)=a1*0._dp
+  A(2,:,:)=a2*0._dp
+  A(3,:,:)=a3
+
+  print*,sum(A,1)
+  print*
   print*,sum(A,2)
+  print*
+  print*,sum(A,3)
 
 
 end program trial
