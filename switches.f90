b0441fe93ea6680bc5c943ccda0632c4219afe5e
cibinjoseph <cibinjoseph92@gmail.com>
2018-04-16 23:22:14 +0530

    testcase

--- switches.f90
+++ switches.f90
@@ -1,7 +1,7 @@
 ! This file contains the switches that control certain features in the code
 
 ! Span discretization : [1]linear [2]cosine [3]halfsine
-integer, parameter :: span_spacing_switch=3
+integer, parameter :: span_spacing_switch=1
 
 ! Wake tip dissipation: [0]Off [1]On
 integer, parameter :: tip_diss_switch=0
@@ -17,7 +17,7 @@ integer, parameter :: wakestrain_switch=0
 integer, parameter :: wakeplot_switch=2
 
 ! Wake convection finite-difference scheme [0]FT1 [1]PC2 [2]AB2 [3]AB4
-integer, parameter :: FDscheme_switch=2
+integer, parameter :: FDscheme_switch=3
 
 ! Ignore Wake wake interaction [0]Off [1..]timesteps
 integer, parameter :: wake_ignore_nt=0
