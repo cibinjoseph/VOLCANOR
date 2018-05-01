2a05baa4f59e95ab8a471d0b620ce3e3af38717b
cibinjoseph <cibinjoseph92@gmail.com>
2018-05-01 15:12:08 +0530

    axisrotate trials

--- trial.f90
+++ trial.f90
@@ -3,8 +3,9 @@ program trial
   implicit none 
   integer, parameter :: nx=200
   real(dp), dimension(3,nx) :: A
-  real(dp) :: t
-  real(dp) :: pivotLE
+  real(dp), dimension(3) :: u
+  real(dp) :: t, theta,ct,st,onect
+  real(dp), dimension(3,3) :: Tmat
   integer :: i
 
   ! Construct helix
@@ -28,9 +29,33 @@ program trial
 
 
   ! Specify axis of rotation
+  u=(/1._dp,0._dp,1._dp/)
+  u=u/norm2(u)
+  print*,u
+
+  theta=60._dp*pi/180._dp
+  ct=cos(theta)
+  st=sin(theta)
+  onect=1-ct
 
   ! Rotate
+  Tmat(:,1)=(/      ct+u(1)*u(1)*onect,  u(3)*st+u(2)*u(1)*onect, -u(2)*st+u(3)*u(1)*onect/)
+  Tmat(:,2)=(/-u(3)*st+u(1)*u(2)*onect,       ct+u(2)*u(2)*onect,  u(1)*st+u(3)*u(2)*onect/)
+  Tmat(:,3)=(/ u(2)*st+u(1)*u(3)*onect, -u(1)*st+u(2)*u(3)*onect,       ct+u(3)*u(3)*onect/)
+   
+  do i=1,nx
+    A(:,i)=matmul(Tmat,A(:,i))
+  enddo
 
   ! Write out rotated helix
+  open(unit=10,file='ahelix2.tec')
+  write(10,*) 'Title = Helix'
+  write(10,*) 'Variables = "X" "Y" "Z"'
+  write(10,*) 'Zone I=200 J=1 K=1'
+  do i=1,nx
+    write(10,*)A(1,i),A(2,i),A(3,i)
+  enddo
+  close(10)
+
 
 end program trial
