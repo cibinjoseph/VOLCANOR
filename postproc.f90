982e81d750dfd42a92fa50058cf130fb2f340260
cibinjoseph <cibinjoseph92@gmail.com>
2018-05-15 17:21:40 +0530

    Add rotor2file()

--- postproc.f90
+++ postproc.f90
@@ -1,8 +1,79 @@
 module postproc
-  use wingpanel_classdef
-  use wakepanel_classdef
+  use rotor_classdef
 
 contains
+
+  subroutine rotor2file(rotor,row_now,filename)
+    type(wakepanel_class), intent(in) :: rotor
+    integer, intent(in) :: row_now
+    character(len=*), intent(in) :: filename
+    character(len=5) :: nx_char, ny_char
+    real(dp), dimension(3,rotor%nc+1,rotor%ns+1) :: wing_mesh  
+    real(dp), dimension(3,size(waP,1)+1,rotor%ns+1) :: wake_mesh  
+    integer :: i,j,nx,ny,ib
+
+    nx=rotor%nc
+    ny=rotor%ns
+    write(nx_char,'(I5)') nx+1
+    write(ny_char,'(I5)') ny+1
+
+    open(unit=10,file=filename,position='append')
+    write(10,*) 'Title = "Panel array"'
+    write(10,*) 'VARIABLES = "X" "Y" "Z" "GAM" "TAG"'
+
+    do ib=1,nb
+      do j=1,ny
+        do i=1,nx
+          wing_mesh(:,i,j)=rotor%blade(ib)%wiP(i,j)%pc(:,1)
+        enddo
+      enddo
+      do i=1,nx
+        wing_mesh(:,i,ny+1)=rotor%blade(ib)%wiP(i,ny)%pc(:,4)
+      enddo
+      do j=1,ny
+        wing_mesh(:,nx+1,j)=rotor%blade(ib)%wiP(nx,j)%pc(:,2)
+      enddo
+      wing_mesh(:,nx+1,ny+1)=rotor%blade(ib)%wiP(nx,ny)%pc(:,3)
+
+      write(10,*) 'Zone I='//trim(nx_char)//' J='//trim(ny_char)//' K=1  T="Blade"'
+      write(10,*) 'DATAPACKING=BLOCK'
+      write(10,*) 'VARLOCATION=([4]=CELLCENTERED)'!,[5]=CELLCENTERED)'
+      write(10,*) ((wing_mesh(1,i,j),i=1,nx+1),j=1,ny+1)
+      write(10,*) ((wing_mesh(2,i,j),i=1,nx+1),j=1,ny+1)
+      write(10,*) ((wing_mesh(3,i,j),i=1,nx+1),j=1,ny+1)
+      write(10,*) ((-1._dp*rotor%blade(ib)%wiP(i,j)%vr%gam,i=1,nx),j=1,ny)
+
+      nx=size(rotor%blade(1)%waP,1)
+      ny=rotor%ns
+      write(nx_char,'(I5)') nx-(row_now-1)+1
+      write(ny_char,'(I5)') ny+1
+
+      !Check if necessary - $omp parallel do collapse(2)
+      do j=1,ny
+        do i=row_now,nx
+          wake_mesh(:,i,j)=rotor%blade(ib)%waP(i,j)%vr%vf(1)%fc(:,1)
+        enddo
+      enddo
+      !Check if necessary -$omp end parallel do
+      do i=row_now,nx
+        wake_mesh(:,i,ny+1)=rotor%blade(ib)%waP(i,ny)%vr%vf(4)%fc(:,1)
+      enddo
+      do j=1,ny
+        wake_mesh(:,nx+1,j)=rotor%blade(ib)%waP(nx,j)%vr%vf(2)%fc(:,1)
+      enddo
+      wake_mesh(:,nx+1,ny+1)=rotor%blade(ib)%waP(nx,ny)%vr%vf(3)%fc(:,1)
+
+      write(10,*) 'Zone I='//trim(nx_char)//' J='//trim(ny_char)//' K=1  T="Wake"'
+      write(10,*) 'DATAPACKING=BLOCK'
+      write(10,*) 'VARLOCATION=([4]=CELLCENTERED)'!,[5]=CELLCENTERED)'
+      write(10,*) ((wake_mesh(1,i,j),i=row_now,nx+1),j=1,ny+1)
+      write(10,*) ((wake_mesh(2,i,j),i=row_now,nx+1),j=1,ny+1)
+      write(10,*) ((wake_mesh(3,i,j),i=row_now,nx+1),j=1,ny+1)
+      write(10,*) ((-1._dp*rotor%blade(ib)%waP(i,j)%vr%gam,i=row_now,nx),j=1,ny)
+    enddo
+
+    close(10)
+  end subroutine rotor2file
 
   subroutine mesh2file(wing_array,wake_array,filename)
     type(wingpanel_class), intent(in), dimension(:,:) :: wing_array
