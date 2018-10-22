20551ee7fd1118db1690ca333ace9525f74bcc02
cibinjoseph <cibinjoseph92@gmail.com>
2018-10-22 17:46:19 +0530

    Use signal handlers

--- plotit.py
+++ plotit.py
@@ -3,19 +3,37 @@
 
 import visit
 import argparse
-import os
+import signal, os, sys
 from subprocess import call
 from time import sleep
 
+# Functions to be invoked for asynchronous keyboard signals ctrl+Z and ctrl+C
+def ctrlZ_func(signum, frame):
+    print('Reloading plots...')
+    os.execl(sys.executable, 'python', __file__, *sys.argv[1:])  # Rerun code
+
+def ctrlC_func(signum, frame):
+    try:
+        os.remove('visitlog.py')
+    except OSError:
+        pass
+    print('Program exit...')  # Exit program
+    sys.exit(0)
+
+# Attach signal to the respective signal handlers (functions)
+signal.signal(signal.SIGTSTP, ctrlZ_func)
+signal.signal(signal.SIGINT, ctrlC_func)
+
+# Define input arguments
 parser = argparse.ArgumentParser(
-        description=('Visualize plots using visit'),
-        epilog='Author: Cibin Joseph')
-parser.add_argument('-w', '--wake', help='Plot wake structure', action='store_true')
-parser.add_argument('-t', '--tip', help='Plot wake tip', action='store_true')
-parser.add_argument('-p', '--panel', help='Plot wing alone', action='store_true')
-parser.add_argument('-g', '--gamma', help='Plot gamma sectional', action='store_true')
-parser.add_argument('-l', '--lift', help='Plot lift', action='store_true')
-parser.add_argument('-d', '--drag', help='Plot drag', action='store_true')
+        description = ('Visualize plots using visit'), 
+        epilog = 'Author: Cibin Joseph')
+parser.add_argument('-w', '--wake', help='Plot wake structure', action = 'store_true')
+parser.add_argument('-t', '--tip', help='Plot wake tip', action = 'store_true')
+parser.add_argument('-p', '--panel', help='Plot wing alone', action = 'store_true')
+parser.add_argument('-g', '--gamma', help='Plot gamma sectional', action = 'store_true')
+parser.add_argument('-l', '--lift', help='Plot lift', action = 'store_true')
+parser.add_argument('-d', '--drag', help='Plot drag', action = 'store_true')
 
 args = parser.parse_args()
 
@@ -35,40 +53,27 @@ elif args.gamma == True:
 
 elif args.lift == True:
     filename = 'plot_lift.py'
-    try:
-        if os.path.exists('Results/lift.curve') == False:
-            print('Waiting for file creation...') 
-        while os.path.exists('Results/lift.curve') == False:
-            sleep(1)
-    except KeyboardInterrupt:
-        print(' ...Program Exit')
+    if os.path.exists('Results/lift.curve') == False:
+        print('Waiting for file creation...') 
+    while os.path.exists('Results/lift.curve') == False:
+        sleep(1)
 
 elif args.drag == True:
     filename = 'plot_drag.py'
-    try:
-        if os.path.exists('Results/drag.curve') == False:
-            print('Waiting for file creation...') 
-        while os.path.exists('Results/drag.curve') == False:
-            print('Waiting for file creation...') 
-            sleep(1)
-    except KeyboardInterrupt:
-        print(' ...Program Exit')
+    if os.path.exists('Results/drag.curve') == False:
+        print('Waiting for file creation...') 
+    while os.path.exists('Results/drag.curve') == False:
+        print('Waiting for file creation...') 
+        sleep(1)
 
 else:
-    #print('Error: Wrong input arguments')
-    #raise ValueError
+    # print('Error: Wrong input arguments')
+    # raise ValueError 
     filename = 'plot_wake.py'  # Assume -w flag by dfault
 
 
+call(['visit', '-np', '4', '-s', '{}/{}'.format(src_plot_dir, filename)])
 try:
-    call(['visit','-np','4','-s','{}/{}'.format(src_plot_dir,filename)])
-    try:
-        os.remove('visitlog.py')
-    except OSError:
-        pass
-except KeyboardInterrupt:
-    try:
-        os.remove('visitlog.py')
-    except OSError:
-        pass
-    print(' ...Program Exit')
+    os.remove('visitlog.py')
+except OSError:
+    pass
