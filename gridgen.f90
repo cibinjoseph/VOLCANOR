de3c301085ba1607232d36f903d3545ea7b1c1ac
cibinjoseph <cibinjoseph92@gmail.com>
2018-10-24 11:57:36 +0530

    Add filename ranges

--- gridgen.f90
+++ gridgen.f90
@@ -3,11 +3,13 @@ program gridgen
 
   integer :: nx,ny,nz
   real(dp), dimension(3) :: Cmin, Cmax    ! Coordinates of corners
+  integer :: filerange_start, filerange_step, filerange_end
 
   integer :: ix,iy,iz,icrd
   real(dp), allocatable, dimension(:) :: xvec,yvec,zvec
   real(dp), allocatable, dimension(:,:,:,:) :: grid, grid_centre
   character(len=5) :: nx_char,ny_char,nz_char
+  character(len=5) :: timestamp
 
   ! Read gridconfig.in file
   call print_status('Reading file '//'gridconfig.in')
@@ -18,14 +20,22 @@ program gridgen
   read(11,*) Cmin(1),Cmin(2),Cmin(3)
   call skiplines(11,3)
   read(11,*) Cmax(1),Cmax(2),Cmax(3)
+  call skiplines(11,5)
+  read(11,*) filerange_start, filerange_step, filerange_end
   close(11)
 
-  ! Sanity check
+  ! Sanity check for Cmin and Cmax values
   if (Cmin(1)>Cmax(1) .or. Cmin(2)>Cmax(2) .or. Cmin(3)>Cmax(3)) then
     error stop 'ERROR: All XYZmin values should be greater than XYZmax values'
   endif
 
   call print_status()    ! SUCCESS
+
+  ! Read from filaments file
+  write(timestamp,'(I0.5)') filerange_start
+  open(unit=12,file='Results/filaments'//timestamp//'.dat')
+
+  close(12)
 
   ! Allocate grid coordinates
   allocate(grid(3,nx,ny,nz))
@@ -63,22 +73,22 @@ program gridgen
   grid_centre=grid_centre*0.125_dp
 
   ! Write to file
-
   call print_status('Writing to grid file')
-  open(unit=12,file='Results/grid01000.tec')
+  write(timestamp,'(I0.5)') filerange_start
+  open(unit=13,file='Results/grid'//timestamp//'.dat')
 
-  write(12,*) 'TITLE = "Grid"'
-  write(12,*) 'VARIABLES = "X" "Y" "Z" "U-vel" "V-vel" "W-vel"'
-  write(12,*) 'ZONE I='//trim(nx_char)//' J='//trim(ny_char)//' K='//trim(nz_char)//' T="Data"'
-  write(12,*) 'DATAPACKING=BLOCK'
-  write(12,*) 'VARLOCATION=([4]=CELLCENTERED,[5]=CELLCENTERED,[6]=CELLCENTERED)'
-  write(12,*) (((grid(1,ix,iy,iz),ix=1,nx),iy=1,ny),iz=1,nz) 
-  write(12,*) (((grid(2,ix,iy,iz),ix=1,nx),iy=1,ny),iz=1,nz) 
-  write(12,*) (((grid(3,ix,iy,iz),ix=1,nx),iy=1,ny),iz=1,nz) 
-  write(12,*) (((grid_centre(1,ix,iy,iz),ix=1,nx-1),iy=1,ny-1),iz=1,nz-1)
-  write(12,*) (((grid_centre(2,ix,iy,iz),ix=1,nx-1),iy=1,ny-1),iz=1,nz-1)
-  write(12,*) (((grid_centre(3,ix,iy,iz),ix=1,nx-1),iy=1,ny-1),iz=1,nz-1)
-  close(12)
+  write(13,*) 'TITLE = "Grid"'
+  write(13,*) 'VARIABLES = "X" "Y" "Z" "U-vel" "V-vel" "W-vel"'
+  write(13,*) 'ZONE I='//trim(nx_char)//' J='//trim(ny_char)//' K='//trim(nz_char)//' T="Data"'
+  write(13,*) 'DATAPACKING=BLOCK'
+  write(13,*) 'VARLOCATION=([4]=CELLCENTERED,[5]=CELLCENTERED,[6]=CELLCENTERED)'
+  write(13,*) (((grid(1,ix,iy,iz),ix=1,nx),iy=1,ny),iz=1,nz) 
+  write(13,*) (((grid(2,ix,iy,iz),ix=1,nx),iy=1,ny),iz=1,nz) 
+  write(13,*) (((grid(3,ix,iy,iz),ix=1,nx),iy=1,ny),iz=1,nz) 
+  write(13,*) (((grid_centre(1,ix,iy,iz),ix=1,nx-1),iy=1,ny-1),iz=1,nz-1)
+  write(13,*) (((grid_centre(2,ix,iy,iz),ix=1,nx-1),iy=1,ny-1),iz=1,nz-1)
+  write(13,*) (((grid_centre(3,ix,iy,iz),ix=1,nx-1),iy=1,ny-1),iz=1,nz-1)
+  close(13)
   call print_status()
 
 end program gridgen
