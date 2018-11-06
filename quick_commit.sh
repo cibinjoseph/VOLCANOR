c1c298d20dcb50ebbc456a74b5a0b94ee571d915
cibinjoseph <cibinjoseph92@gmail.com>
2018-11-06 15:14:03 +0530

    Add flags for quick commit messsages

--- quick_commit.sh
+++ quick_commit.sh
@@ -1,5 +1,11 @@
-if [ $# -eq 0 ]; then  # No arguments
+if [ $# -eq 0 ]; then      # No arguments
   git commit -am "Quick commit"
+elif [[ $1 == '-t' ]] | [[ $1 == 't' ]]; then
+  git commit -am 'testcase'
+elif [[ $1 == '-a' ]] | [[ $1 == 'a' ]]; then
+  git commit todo -m 'Add task'
+elif [[ $1 == '-r' ]] | [[ $1 == 'r' ]]; then
+  git commit todo -m 'Remove task'
 else
   git commit -am "$1"
 fi
