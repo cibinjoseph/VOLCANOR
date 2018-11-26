60da736a4b1dbfda258ca6b167b6234934e5bb8e
cibinjoseph <cibinjoseph92@gmail.com>
2018-11-26 15:52:20 +0530

    Add T_matrices in code itself

--- mymathlib.f90
+++ mymathlib.f90
@@ -275,6 +275,16 @@ contains
   !--------------------------------------------------------!
   !              Transformation Functions                  !
   !--------------------------------------------------------!
+  ! Code to generate Transformation matrices in Octave
+  !
+  ! clc; clear;
+  ! pkg load symbolic;
+  ! syms p t s
+  ! Rp=[[1,0,0];[0,cos(p),sin(p)];[0,-sin(p),cos(p)]];
+  ! Rt=[[cos(t),0,-sin(t)];[0,1,0];[sin(t),0,cos(t)]];
+  ! Rs=[[cos(s),sin(s),0];[-sin(s),cos(s),0];[0,0,1]];
+  ! Tbg=Rp*Rt*Rs
+  ! Tgb=Rs'*Rt'*Rp'
 
   ! Transformation matrix bg
   function Tbg(cs_phi,cs_theta,cs_psi)
