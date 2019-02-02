4b6de6d79526b1879e9913b8883a1bfe0378b0d2
cibinjoseph <cibinjoseph92@gmail.com>
2019-02-02 15:50:47 +0530

    Add lsq subroutine

--- trial.f90
+++ trial.f90
@@ -1,5 +1,10 @@
 program trial
-  open(61,file='data.txt',action='write',position='append')
-  write(61,*) 'hey'
-  close(61)
+  use mymathlib
+  real(dp), dimension(6) :: xData, yData 
+  real(dp), dimension(3) :: out
+  xData=(/0._dp, 0.5_dp, 1._dp, 1.5_dp, 2._dp, 2.5_dp/)
+  yData=(/0.0674_dp, -0.9156_dp, 1.6253_dp, 3.0377_dp, 3.3535_dp, 7.9409_dp/)
+
+  out=lsq2((/0._dp,1._dp,2._dp/),xData,yData)
+  print*,out
 end program trial
