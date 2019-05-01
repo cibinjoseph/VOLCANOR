4c438e5eddadead25b067c76247412e1a7ffa5ee
cibinjoseph <cibinjoseph92@gmail.com>
2019-05-01 11:32:08 +0530

    testcase

--- getCT.m
+++ getCT.m
@@ -1,13 +1,17 @@
 clc; clear; clf;
-A = dlmread('forceDist.curve');
-r = A(2:end,1);
-fz = A(2:end,2);
+A = dlmread('Results/r01forceHist.txt');
+iter = A(2:end,1);
+ct = A(2:end,2);
 
 radius = 1.143;
-omega = 62.83;
-rho = 1.2;
+omega = 130.90;
+rho = 1.225;
 vtip = radius*omega;
+dt = 0.00138;
 
-rbar = r/radius;
-ct = fz/(pi*rho*vtip*vtip*radius*radius)
-plot(rbar,ct)
+nrev = iter*dt*omega/(2*pi);
+%rbar = r/radius;
+%ct = fz/(pi*rho*vtip*vtip*radius*radius)
+%plot(rbar,ct)
+plot(nrev,ct)
+
