5239f7d020d08285c9eca492815ca4ebd7c67c76
cibinjoseph <cibinjoseph92@gmail.com>
2019-05-08 02:09:03 +0530

    testcase

--- tools/getAvgCT.m
+++ tools/getAvgCT.m
@@ -1,6 +1,6 @@
 dataMat = dlmread('Results/r01forceHist.txt');
 dataMat(1,:) = [];
 CT = dataMat(:,2);
-indxStart = length(CT)-900;  % Last 5 revs
+indxStart = length(CT)-180;  % Last 5 revs
 avgCT = mean(CT(indxStart:end));
 disp(avgCT)
