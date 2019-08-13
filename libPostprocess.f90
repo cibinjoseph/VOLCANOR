22fe7a10147d50e26b3194328739f09581a85805
cibinjoseph <cibinjoseph92@gmail.com>
2019-08-13 11:52:11 +0530

    Correct ref location of gam

--- libPostprocess.f90
+++ libPostprocess.f90
@@ -543,18 +543,26 @@ contains
       ic=1
       write(rowNumberChar,'(I0.2)') ic
       write(12,*) '# Blade'//bladeNumberChar//'Row'//rowNumberChar
+      ! DEBUG
       do is=1,rotor%ns
-        write(12,*) norm2(rotor%hubCoords-rotor%blade(ib)%wiP(ic,is)%cp), &
-          -1._dp*rotor%blade(ib)%wiP(ic,is)%vr%gam
+        write(12,*) rotor%blade(ib)%wiP(1,is)%CP(2), -1._dp*sum(rotor%blade(ib)%wiP(:,is)%vr%gam)
       enddo
-      do ic=2,rotor%nc
-        write(rowNumberChar,'(I0.2)') ic
-        write(12,*) '# Blade'//bladeNumberChar//'Row'//rowNumberChar
-        do is=1,rotor%ns
-          write(12,*) norm2(rotor%hubCoords-rotor%blade(ib)%wiP(ic,is)%cp), &
-            -1._dp*(rotor%blade(ib)%wiP(ic,is)%vr%gam-rotor%blade(ib)%wiP(ic-1,is)%vr%gam)
-        enddo
-      enddo
+
+      !ic=1
+      !write(rowNumberChar,'(I0.2)') ic
+      !write(12,*) '# Blade'//bladeNumberChar//'Row'//rowNumberChar
+      !do is=1,rotor%ns
+      !  write(12,*) norm2(rotor%hubCoords-rotor%blade(ib)%wiP(ic,is)%cp), &
+      !    -1._dp*rotor%blade(ib)%wiP(ic,is)%vr%gam
+      !enddo
+      !do ic=2,rotor%nc
+      !  write(rowNumberChar,'(I0.2)') ic
+      !  write(12,*) '# Blade'//bladeNumberChar//'Row'//rowNumberChar
+      !  do is=1,rotor%ns
+      !    write(12,*) norm2(rotor%hubCoords-rotor%blade(ib)%wiP(ic,is)%cp), &
+      !      -1._dp*(rotor%blade(ib)%wiP(ic,is)%vr%gam-rotor%blade(ib)%wiP(ic-1,is)%vr%gam)
+      !  enddo
+      !enddo
     enddo
     close(12)
 
