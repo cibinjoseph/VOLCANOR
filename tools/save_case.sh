7676821a9f7add222ce850fc9e91ba730e7af5d1
cibinjoseph <cibinjoseph92@gmail.com>
2019-10-23 19:07:39 +0530

    Add tool to save case files

--- tools/save_case.sh
+++ tools/save_case.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
-# Code to save parameters from current case
-# Redirect output to required file
+# Code to save inputs and output from current case
 
-# Parameters from config file and rotor files
-cat <(echo '==> config.in <==') config.in <(echo)\
-  <(echo '==> rotor??.in <==') rotor??.in
+set -e
+
+if [ $# -eq 0 ]; then
+  echo "Error: No arguments supplied"
+  exit 1
+fi
+
+# Make folder provided as argument
+recordDir=$1
+mkdir -p $recordDir
+
+# Copy input files
+echo "Copying input files ..."
+cp *.in $recordDir
+
+# Copy output files
+echo "Copying output files ..."
+cp -r Results $recordDir
+
+# Get airfoil used
+airfoilName="$(awk 'END {print $2}' rotor01.in)"
+ls airfoils/$airfoilName > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Copying airfoil files ... $airfoilName"
+  cp airfoils/$airfoilName $recordDir
+fi
+
+# Get geometry used
+geometryName="$(head -n3 rotor01.in | awk 'END {print $2}')"
+ls geometry/$geometryName > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Copying geometry files ... $geometryName"
+  cp geometry/$geometryName $recordDir
+fi
+
+# Check for multiple rotor files
+numOfrotorFiles="$(ls rotor??.in | wc -l)"
+if [ $numOfrotorFiles -gt 1 ]; then
+  echo "Warning: Multiple rotor files found. Copy others separately"
+fi
