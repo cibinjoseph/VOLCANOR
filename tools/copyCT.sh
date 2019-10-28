56faa76ba250e364ba6fc5d31cd66e0071c7066c
cibinjoseph <cibinjoseph92@gmail.com>
2019-10-28 12:09:32 +0530

    Input arguments to copy line

--- tools/copyCT.sh
+++ tools/copyCT.sh
@@ -1,2 +1,13 @@
-tail -n 3 Results/r01forceHist.txt | head -n 1 | awk '{print $2}' | xsel -ib
-tail -n 3 Results/r01forceHist.txt | head -n 1
+#! /bin/bash
+
+# This script copies CT from Results/r01forceHist*.txt to clipboard
+
+# Check for input arguments
+if [[ $# -eq 0 ]]; then
+  lineFromLast=1
+else
+  lineFromLast=$1
+fi
+
+tail -n $lineFromLast Results/r01forceHist.txt | head -n 1 | awk '{print $2}' | xsel -ib
+tail -n $lineFromLast Results/r01forceHist.txt | head -n 1
