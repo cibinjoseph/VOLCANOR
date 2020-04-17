d74182d23e1a8da1654c2ab41536759c287caf54
cibinjoseph <cibinjoseph92@gmail.com>
2020-04-18 02:36:46 +0530

    Add tolerance from classdef for comparison

--- tests/panel2_test.f90
+++ tests/panel2_test.f90
@@ -9,52 +9,55 @@ module panel2_test
 
 contains
 
-  subroutine setUp()
+  subroutine setup()
+    ! Global vaariables
+    nt = 5
+    dt = 0.00625_dp
+    density = 1._dp
 
-  rotor%nb = 1
-  rotor%geometryFile = '0'
+    rotor%nb = 1
+    rotor%geometryFile = '0'
 
-  rotor%nc = 1
-  rotor%ns = 2
-  rotor%nNwake = 2
+    rotor%nc = 1
+    rotor%ns = 2
+    rotor%nNwake = 2
 
-  rotor%hubCoords = (/0._dp, 0._dp, 0._dp/)
-  rotor%cgCoords = (/0._dp, 0._dp, 0._dp/)
-  rotor%fromCoords = (/0._dp, 0._dp, 0._dp/)
-  rotor%pts = (/0._dp, 0._dp, 0._dp/)
+    rotor%hubCoords = (/0._dp, 0._dp, 0._dp/)
+    rotor%cgCoords = (/0._dp, 0._dp, 0._dp/)
+    rotor%fromCoords = (/0._dp, 0._dp, 0._dp/)
+    rotor%pts = (/0._dp, 0._dp, 0._dp/)
 
-  rotor%radius = 2._dp
-  rotor%root_cut = 0._dp
-  rotor%chord = 1._dp
-  rotor%coningAngle = 0._dp
+    rotor%radius = 2._dp
+    rotor%root_cut = 0._dp
+    rotor%chord = 1._dp
+    rotor%coningAngle = 0._dp
 
-  rotor%Omega = 0._dp
-  rotor%shaftAxis = (/0._dp, 0._dp, 0._dp/)
-  rotor%controlPitch = (/5._dp, 0._dp, 0._dp/)
-  rotor%thetaTwist = 0._dp
-  rotor%velBody = (/-10._dp, 0._dp, 0._dp/)
-  rotor%omegaBody = (/0._dp, 0._dp, 0._dp/)
-  rotor%pivotLE = 0.25_dp
-  rotor%flapHinge = 0._dp
-  rotor%symmetricTau = 1._dp
-  rotor%turbulentViscosity = 5._dp
+    rotor%Omega = 0._dp
+    rotor%shaftAxis = (/0._dp, 0._dp, 0._dp/)
+    rotor%controlPitch = (/5._dp, 0._dp, 0._dp/)
+    rotor%thetaTwist = 0._dp
+    rotor%velBody = (/-10._dp, 0._dp, 0._dp/)
+    rotor%omegaBody = (/0._dp, 0._dp, 0._dp/)
+    rotor%pivotLE = 0.25_dp
+    rotor%flapHinge = 0._dp
+    rotor%symmetricTau = 1._dp
+    rotor%turbulentViscosity = 5._dp
 
-  rotor%spanwiseCore = 0.04_dp
-  allocate(rotor%streamwiseCoreVec(rotor%ns+1))
-  rotor%streamwiseCoreVec = 0.04_dp
+    rotor%spanwiseCore = 0.04_dp
+    allocate(rotor%streamwiseCoreVec(rotor%ns+1))
+    rotor%streamwiseCoreVec = 0.04_dp
 
-  rotor%rollupStartRadius = 0.75_dp
-  rotor%rollupEndRadius = 1._dp
-  rotor%initWakeVel = 0._dp
-  rotor%psiStart = 0._dp
-  rotor%skewLimit = 0.5_dp
-  rotor%dragUnitVec = 0._dp
-  rotor%sideUnitVec = 0._dp
-  rotor%liftUnitVec = 0._dp
+    rotor%rollupStartRadius = 0.75_dp
+    rotor%rollupEndRadius = 1._dp
+    rotor%initWakeVel = 0._dp
+    rotor%psiStart = 0._dp
+    rotor%skewLimit = 0.5_dp
+    rotor%dragUnitVec = 0._dp
+    rotor%sideUnitVec = 0._dp
+    rotor%liftUnitVec = 0._dp
 
-  rotor%nAirfoils = 0
-
-  end subroutine setUp
+    rotor%nAirfoils = 0
+  end subroutine setup
 
   subroutine test_aic()
     integer :: spanSpacingSwitch = 2
@@ -67,7 +70,8 @@ contains
     AIC(1, :) = (/1.1223476_dp, -9.2667149E-002_dp/)
     AIC(2, :) = (/-9.2667149E-002_dp, 1.1223476_dp/)
 
-    call assert_equal(AIC, rotor%AIC, 1E-6_dp, 'AICs do not match')
+    call rotor%calcAIC()
+    call assert_equal(AIC, rotor%AIC, tol, 'AICs do not match')
 
     call testcase_finalize()
   end subroutine test_aic
