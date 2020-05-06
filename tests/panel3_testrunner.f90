2cc4afaa04d0c951a755b645bcb586e8827c980b
cibinjoseph <cibinjoseph92@gmail.com>
2020-05-06 14:53:24 +0530

    Add 3-panel tests

---
+++ tests/panel3_testrunner.f90
@@ -0,0 +1,14 @@
+program panel2_testrunner
+  use panel2_test
+  integer :: exit_code
+
+  call testsuite_initialize()
+
+  call setup()
+  call test_aic()
+  call test_force_gamma()
+
+  call testsuite_summary()
+  call testsuite_finalize(exit_code)
+  call exit(exit_code)
+end program panel2_testrunner
