Add usage and correct angle units

tools/wingFELS.py
@@ -7,6 +7,10 @@ import numpy as np
 
 # Get filenames as arguments
 args = sys.argv
+if len(args) == 1:
+    print('Usage: wingFELS.py a.C81 r01ForceDistxx.dat r01Params.dat')
+    quit()
+
 for arg in args[1:]:
     if '.C81' in arg:
         c81File = arg
@@ -28,7 +32,7 @@ with open(c81File, 'r') as fh:
 secAlpha = []
 secCL_nonLin = []
 for CL_Lin in secCL:
-    alpha = (CL_Lin - CL0)/CLa
+    alpha = (180.0/np.pi)*(CL_Lin - CL0)/CLa
     secAlpha.append(alpha)
     CL_nonLin = c81Airfoil.getCL(alpha, 0.1)
     secCL_nonLin.append(CL_nonLin)
@@ -36,4 +40,5 @@ for CL_Lin in secCL:
 wingCL_nonLin = np.dot(np.array(secCL_nonLin), secArea) / refArea
 
 print(('Net CL =', wingCL_nonLin))
-print(secCL_nonLin)
+for indx, secCL in enumerate(secCL_nonLin):
+    print((secAlpha[indx], secCL))