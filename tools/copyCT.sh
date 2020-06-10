bb1287e53ac77dc31ab995ce63fc54b253a4ca49
cibinjoseph <cibinjoseph92@gmail.com>
2020-06-10 13:57:20 +0530

    Correct name in copyCT tool

--- tools/copyCT.sh
+++ tools/copyCT.sh
@@ -2,12 +2,14 @@
 
 # This script copies CT from Results/r01forceHist*.txt to clipboard
 
+forceFile="r01ForceNonDim.dat"
+
 # Check for input arguments
 if [[ $# -eq 0 ]]; then
-  lineFromLast=1
+  resultsDir="Results"
 else
-  lineFromLast=$1
+  resultsDir=$1
 fi
 
-tail -n $lineFromLast Results/r01forceHist.txt | head -n 1 | awk '{print $2}' | xsel -ib
-tail -n $lineFromLast Results/r01forceHist.txt | head -n 1
+tail -n 1 $resultsDir/$forceFile | head -n 1 | awk '{print $2}' | xsel -ib
+tail -n 1 $resultsDir/$forceFile | head -n 1
