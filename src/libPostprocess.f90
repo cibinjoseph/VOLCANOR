a41e061f0561d4ab621597370921fbb35429576f
cibinjoseph <cibinjoseph92@gmail.com>
2020-08-05 12:27:01 +0530

    Add remaining params to file

--- src/libPostprocess.f90
+++ src/libPostprocess.f90
@@ -51,6 +51,7 @@ contains
     write(10, *) 'dt', dt
     write(10, *) 'nr', nr
     write(10, *) 'nb', rotor%nb
+    write(10, *) 'propConvention', rotor%propConvention
     write(10, *) 'geometryFile ', rotor%geometryFile
     write(10, *) 'nc', rotor%nc
     write(10, *) 'ns', rotor%ns
@@ -59,7 +60,13 @@ contains
     write(10, *) 'root_cut', rotor%root_cut
     write(10, *) 'chord', rotor%chord
     write(10, *) 'Omega', rotor%Omega
+    write(10, *) 'phi', rotor%pts(1)
+    write(10, *) 'theta', rotor%pts(2)
+    write(10, *) 'psi', rotor%pts(3)
     write(10, *) 'theta0', rotor%controlPitch(1)*180._dp/pi
+    write(10, *) 'u', rotor%velBody(1)
+    write(10, *) 'v', rotor%velBody(2)
+    write(10, *) 'w', rotor%velBody(3)
     write(10, *) 'nonDimForceDenom', rotor%nonDimforceDenominator
     if (rotor%nAirfoils .gt. 0) then
       write(10, *) 'CL0', rotor%CL0(1)
