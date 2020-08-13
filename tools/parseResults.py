ef0caced98e920048042b4381dc3507bda4aff0b
cibinjoseph <cibinjoseph92@gmail.com>
2020-08-14 04:55:49 +0530

    Mangle unused functions to private

--- tools/parseResults.py
+++ tools/parseResults.py
@@ -4,11 +4,30 @@ from glob import glob
 
 ResultsDir = 'Results/'
 
-def getHeader(file):
+def _getHeader(file):
     ''' Returns variables in header as list of strings '''
     with open(file, 'r') as fh:
         line = fh.readline()
     return line.split()
+
+def _getDataDict(file):
+    """ Parse File and extract header and dataDict """
+    mat = np.loadtxt(file, skiprows=1)
+    dataDict = {}
+    for col, var in enumerate(_getHeader(file)):
+        dataDict[var] = mat[:, col]
+    return dataDict
+
+def _getLatestFile(filename):
+    """ Return filename of latest file with starting string """
+    fileNumPrev = 0
+    filenameLen = len(filename)
+    for currentFile in glob(filename+'*'):
+        fileNum = int(currentFile[filenameLen:filenameLen+5])
+        if fileNum > fileNumPrev:
+            latestFile = currentFile
+            fileNumPrev = fileNum
+    return latestFile
 
 def getParams(file=ResultsDir + 'r01Params.dat'):
     """ Extract parameters from params file """
@@ -21,37 +40,18 @@ def getParams(file=ResultsDir + 'r01Params.dat'):
         params[cols[0]] = cols[1]
     return params
 
-def getData(file):
-    """ Parse File and extract header and data """
-    mat = np.loadtxt(file, skiprows=1)
-    data = {}
-    for col, var in enumerate(getHeader(file)):
-        data[var] = mat[:, col]
-    return data
-
-def getLatestFile(filename):
-    """ Return filename of latest file with starting string """
-    fileNumPrev = 0
-    filenameLen = len(filename)
-    for currentFile in glob(filename+'*'):
-        fileNum = int(currentFile[filenameLen:filenameLen+5])
-        if fileNum > fileNumPrev:
-            latestFile = currentFile
-            fileNumPrev = fileNum
-    return latestFile
-
 def getForceDist(file=None):
-    """ Extract data from forcedist file """
+    """ Extract dataDict from forcedist file """
     if file == None:
         # Parse ResultsDir and get latest ForceDist file
-        file = getLatestFile(ResultsDir + 'r01b01ForceDist')
-    return getData(file)
+        file = _getLatestFile(ResultsDir + 'r01b01ForceDist')
+    return _getDataDict(file)
 
 
 def getForceDim(file=ResultsDir + 'r01ForceDim.dat'):
-    """ Extract data from ForceDim file """
-    return getData(file)
+    """ Extract dataDict from ForceDim file """
+    return _getDataDict(file)
 
 def getForceNonDim(file=ResultsDir + 'r01ForceNonDim.dat'):
-    """ Extract data from ForceDim file """
-    return getData(file)
+    """ Extract dataDict from ForceDim file """
+    return _getDataDict(file)
