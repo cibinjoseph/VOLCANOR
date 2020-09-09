a28228dcdc01463de5409f619885c457fc0ab557
cibinjoseph <cibinjoseph92@gmail.com>
2020-09-09 15:19:37 +0530

    Add alpha in forcedist file

--- naca5605.case/compareAlpha.py
+++ naca5605.case/compareAlpha.py
@@ -4,27 +4,28 @@ import numpy as np
 import parseResults as pr
 import matplotlib.pyplot as plt
 
-# read alphadist
-data = pr._getDataDict(pr.ResultsDir + 'r01b01alphaDist00300.curve')
-secSpan = np.array(data['secSpan'])
-secAlpha = np.array(data['alpha'])
-plt.plot(secSpan, secAlpha, 'b-.', label='IndVel')
+alpha0_deg = -4.154481
 
-# read forcedist
+CLa = 2.0*np.pi  # per rad
+alpha0 = alpha0_deg * np.pi/180.0
+
+# read forcedist file
 data = pr.getForceDist()
 secSpan = np.array(data['secSpan'])
 secCL = np.array(data['secCL'])
-CLa = 6.2832  # per rad
-CL0 = 0.7106
-secAlphaCalc = (secCL-CL0)/CLa*(180.0/np.pi)
-plt.plot(secSpan, secAlphaCalc, 'r-.', label='FELS')
-print('Approx. alpha = ' + str(secAlphaCalc[int(len(secAlphaCalc)/2)]))
+secAlpha = np.array(data['secAlpha'])
+
+secAlphaEst = (secCL/CLa + alpha0)*(180.0/np.pi)
+
+plt.plot(secSpan, secAlpha, 'b-.', label='IndVel')
+plt.plot(secSpan, secAlphaEst, 'r-.', label='FELS')
+print('Approx. alpha = ' + str(secAlphaEst[int(len(secAlphaEst)/2)]))
 
 # read vsp forcedist
 vspMat = np.loadtxt('vsp.dat')
 yavg = vspMat[:, 0]
 CL = vspMat[:, 1]
-secAlphaVSP = (CL-CL0)/CLa*(180.0/np.pi)
+# secAlphaVSP = (CL-CL0)/CLa*(180.0/np.pi)
 # plt.plot(yavg, secAlphaVSP, 'g.', label='VSP')
 
 plt.ylabel('sectional alpha (deg)')
