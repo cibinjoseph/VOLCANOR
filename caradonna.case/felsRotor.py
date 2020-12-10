e1c8031e948c9a0a1bf7b7a5049d6e8c8ee9f1af
cibinjoseph <cibinjoseph92@gmail.com>
2020-12-11 01:06:33 +0530

    Add felsrotor script

--- caradonna.case/felsRotor.py
+++ caradonna.case/felsRotor.py
@@ -8,32 +8,27 @@ import matplotlib.pyplot as plt
 params = pr.getParams()
 data = pr.getForceDist()
 
+# Get variables to global scope
+locals().update(params)
+locals().update(data)
+
 CLa_lin = 2.0*np.pi
 alf0_deg = 0.0
 
 alf0 = alf0_deg*np.pi/180.0
-omega = params['Omega']
-rho = params['density']
-Rad = params['radius']
-nb = params['nb']
 
-rhoFELS = rho
-vTip = Rad*omega
-
-secSpan = data['secSpan']
-secCL = data['secCL']
-secArea = data['secArea']
-secAlpha = data['secAlpha']
-dx = secArea/data['secChord']
-
-vinf = secSpan*omega
+densityFELS = density
+vTip = radius*Omega
+dx = secArea/secChord
+vinf = secSpan*Omega
 
 alphalist = (180.0/np.pi)*(secCL/CLa_lin + alf0)
+machlist = secVel/velSound
 # print(alphalist)
 
 fig, ax = plt.subplots(2)
-ax[0].plot(secSpan/Rad, alphalist, 'b*-', label='FELS')
-ax[0].plot(secSpan/Rad, secAlpha, 'r*-', label='Ind. vel.')
+ax[0].plot(secSpan/radius, alphalist, 'b*-', label='FELS')
+ax[0].plot(secSpan/radius, secAlpha, 'r*-', label='Ind. vel.')
 ax[0].legend()
 ax[0].set_ylabel('Alpha (deg)')
 ax[0].grid(True)
@@ -43,23 +38,27 @@ with open(c81File, 'r') as fh:
     c81Airfoil = c81.load(fh)
 
 CL_nonlin = []
-for alpha in alphalist:
-    CL_nonlin.append(c81Airfoil.getCL(alpha, 0.2))
+for i, alpha in enumerate(alphalist):
+    CL_nonlin.append(c81Airfoil.getCL(alpha, machlist[i]))
 
-secLift = CL_nonlin*(0.5*rhoFELS*secArea*vinf*vinf)
+secLift = CL_nonlin*(0.5*densityFELS*secArea*vinf*vinf)
 ThrustFELS = nb*np.sum(secLift)
-CTFELS = ThrustFELS / (rhoFELS*np.pi*Rad*Rad*(vTip)**2.0)
+CTFELS = ThrustFELS / (densityFELS*np.pi*radius*radius*(vTip)**2.0)
 print('Min/Max alpha (deg) = ' + str(np.min(alphalist)) +' / ' + str(np.max(alphalist)))
 print('Thrust = ' + str(ThrustFELS))
 print('CT = ' + str(CTFELS))
 
 # Write distribution to file
-outMat = np.column_stack((secSpan/Rad, alphalist, secLift/dx))
+outMat = np.column_stack((secSpan/radius, alphalist, secLift/dx))
 np.savetxt('loadVLM.dat', outMat, delimiter='  ')
 
-# ax[1].plot(secSpan/Rad, CL_nonlin, label='FELS')
-ax[1].plot(secSpan/Rad, secLift/dx, label='FELS')
-ax[1].set_ylabel('Lift per unit span')
+# ax[1].plot(secSpan/radius, CL_nonlin, label='FELS')
+# ax[1].plot(secSpan/radius, secLift/dx, label='FELS')
+# ax[1].set_ylabel('Lift per unit span')
+
+denom = density*(np.pi*radius*radius)*vTip*vTip
+ax[1].plot(secSpan/radius, secCL, label='FELS')
+ax[1].set_ylabel('CTx')
 ax[1].grid(True)
 ax[1].legend()
 plt.xlabel('sec. span (r/R)')
