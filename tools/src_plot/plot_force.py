234ff381039935f189678df1d5599fd2ada805af
cibinjoseph <cibinjoseph92@gmail.com>
2021-04-14 03:03:48 +0530

    Add force plots for all rotors

--- tools/src_plot/plot_force.py
+++ tools/src_plot/plot_force.py
@@ -1,30 +1,5 @@
 # Plot using gnuplot
-plot 'Results/r01ForceNonDim.csv' u 1:2 w l
+filelist=system('ls Results/r??ForceNonDim.csv')
+plot for [filename in filelist] filename u 1:2 w l
 pause 1
 reread
-
-## Plot using matplotlib
-# import matplotlib.pyplot as plt
-# import numpy
-# 
-# # Get data points (timestep and Force)
-# filename = 'Results/r01forceHist.txt'
-# timestamp = []
-# CT = []
-# with open(filename) as forceFile:
-#     # Skip first line
-#     next(forceFile)
-#     for line in forceFile:
-#         # Handle empty lines and comments
-#         if line and (line[0][0] != '#'):
-#             data = line.split()
-#             timestamp.append(float(data[0].strip()))
-#             CT.append(float(data[1].strip()))
-# 
-# plt.plot(timestamp, CT, 'b')
-# plt.xlabel('Iteration')
-# plt.ylabel('CT')
-# plt.grid()
-# 
-# plt.show(block=True)
-# # input('Press Enter to exit')
