f9213b131343941c5f7feee84b264d000312d580
cibinjoseph <cibinjoseph92@gmail.com>
2021-05-08 16:31:46 +0530

    Add provision to retain lines from template

--- tools/changeToTemplate.py
+++ tools/changeToTemplate.py
@@ -6,41 +6,67 @@
 import sys
 import time
 
-def changeFile(inputFile, templateFile):
-    with open(inputFile, 'r') as inptF:
-        inptLines = inptF.readlines()
-    # Remove comments from input file
-    onlyInputs = []
-    for line in inptLines:
+
+def getInputLines(inputFile):
+    """ Returns lines ignoring comments """
+    with open(inputFile, 'r') as fh:
+        lines = fh.readlines()
+    inputLines = []
+    for line in lines:
         if line[0] != '#':
-            onlyInputs.append(line)
+            inputLines.append(line)
+
+    return inputLines
+
+def changeFile(inputFile, templateFile, retainTemplateLines=[]):
+    inptLines = getInputLines(inputFile)
+    tmplLines = getInputLines(templateFile)
+
+    # Ensure no. of lines match
+    if retainTemplateLines == []:
+        if len(inptLines) != len(tmplLines):
+            raise ValueError('No. of lines do not match template in: ' \
+                             + inputFile)
 
     with open(templateFile, 'r') as tmplF:
         tmplLines = tmplF.readlines()
 
+    i = 0
     with open(inputFile, 'w') as inptF:
         for line in tmplLines:
+            # Write comments from template file
             if line[0] == '#':
                 inptF.write(line)
             else:
-                inptF.write(onlyInputs[0])
-                del(onlyInputs[0])
-    if len(onlyInputs) != 0:
-        print('Warning: The following input lines have not been used')
-        print(onlyInputs)
+                # If input line,
+                i = i + 1
+                # check if specified to be retained
+                if i in retainTemplateLines:
+                    inptF.write(line)
+                # else copy line from input file
+                else:
+                    inptF.write(inptLines[0])
+                    del(inptLines[0])
     return
 
 args = sys.argv
 
 if len(args) == 1:
-    print('Usage: changeFile file1 [file2 ...] templateFile')
+    print('Usage: changeFile [-l 1,12,...] file1 [file2 ...] templateFile')
     print('Converts input file comments to format given in template file')
-    print('The template file is assumed to be the last in the arguments')
-    print('All inputs of the input file is retained')
+    print('The template file is assumed to be the last in the arguments.')
+    print('All inputs of the input file is retained.')
+    print('Input line no.s specified after -l are copied from the template.')
     quit()
 
-inputFileList = args[1:-1]
-templateFile = args[-1]
+if args[1] == '-l':
+    retainTemplateLines = [int(i) for i in args[2].split(',')]
+    inputFileList = args[3:-1]
+    templateFile = args[-1]
+else:
+    retainTemplateLines = []
+    inputFileList = args[1:-1]
+    templateFile = args[-1]
 
 print('Template file is: ' + templateFile)
 print('Changing files in 5 seconds. Ctrl+C to stop')
@@ -48,4 +74,4 @@ time.sleep(5)
 
 for inputFile in inputFileList:
     print(inputFile)
-    changeFile(inputFile, templateFile)
+    changeFile(inputFile, templateFile, retainTemplateLines)
