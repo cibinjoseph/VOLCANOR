50f218b8aaa0adbd60a5ae11d42ab130cc90b189
cibinjoseph <cibinjoseph92@gmail.com>
2021-05-19 16:15:45 +0530

    Correct return arguments

--- elevate.case/getForceDist.py
+++ elevate.case/getForceDist.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import parseResults as pr
 
-data = pr.getForceDist()
+data, ff = pr.getForceDist()
 radius = 5.0
 dx = data['secArea']/data['secChord']
 density = 0.002377
@@ -12,6 +12,6 @@ r = data['secSpan']/radius
 plt.plot(r, forcedist, 'bo-')
 
 outmat = np.stack((r, forcedist), axis=-1)
-np.savetxt('forcesVOLCANOR.dat', outmat, delimiter='  ')
+np.savetxt('forcesVOLCANOR2.dat', outmat, delimiter='  ')
 
 # plt.show()
