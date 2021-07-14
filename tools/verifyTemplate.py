bdac0d77a8275cf44f3659a2f676f850532a9433
cibinjoseph <cibinjoseph92@gmail.com>
2021-07-15 03:08:38 +0530

    Automatically compare template files without requiring manual update

--- tools/verifyTemplate.py
+++ tools/verifyTemplate.py
@@ -3,58 +3,84 @@
 
 import sys
 import time
+from pathlib import Path
 
-# Version 0.10
-geomInput = [1, 4, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 2, \
-             6, 4, 2, 3, 2, 1, 2, 3, 3, 3, 3, 2, 1, 2, 3]
 
-# Version 0.3
-configInput = [1, 3, 2, 2, 2, 3, 4, 3, 2, 1, 1, 1]
-
-def isValid(filename, refInput):
-    returnVal = True
+def getLineStats(filename):
+    """ Returns lineStats list with number of inputs in each line
+    0 implies a comment
+    """
     with open(filename) as fh:
         lines = fh.readlines()
 
-    l = []
-    isComment = []
+    lineStats = []
+    versionNum = None
     for line in lines:
-        l.append(len(line.split()))
         if line[0] == '#':
-            isComment.append(True)
+            lineStats.append(0)
         else:
-            isComment.append(False)
+            if not versionNum:
+                # versionNum is NONE on the first encounter
+                versionNum = line.strip()
+            numInputs = len(line.split())
+            lineStats.append(numInputs)
 
-    if isComment.count(False) != len(refInput):
+    return lineStats, versionNum
+
+def isValid(filename, refInput):
+    """ Checks if file is of the same template as refInput """
+    returnVal = True
+
+    refLineStats, refVersion = getLineStats(refInput)
+    lineStats, versionNum = getLineStats(filename)
+
+    # Check version numbers
+    if versionNum != refVersion:
+        print('Version mismath', end=': ')
+        print('Expected ' + str(refVersion) + ', found ' + str(versionNum))
+
+    # Check number of lines (neglecting comments)
+    if (len(refLineStats)-refLineStats.count(0)) != \
+    (len(lineStats)-lineStats.count(0)):
         print('No. of lines mismatch')
         returnVal = False
 
-    refIndex = 0
-    for i in range(len(l)):
-        if isComment[i]:
-            pass
-        else:
-            if l[i] != refInput[refIndex]:
-                print('Mismatch on line ' + str(i+1), end=': ')
-                print('Expected ' + str(refInput[refIndex]) + \
-                      ', found ' + str(l[i]))
-                returnVal = False
+    # Input file stats without comments
+    refLineStatsInpts = [x for x in refLineStats if x != 0]
+
+    refIndex = -1
+    for i, numInputs in enumerate(lineStats):
+        if numInputs != 0:  # Non comment
             refIndex += 1
+            if numInputs != refLineStatsInpts[refIndex]:
+                print('Mismatch on line ' + str(i+1), end=': ')
+                print('Expected ' + str(refLineStatsInpts[refIndex]) + \
+                      ', found ' + str(numInputs))
+                returnVal = False
 
     return returnVal
 
 
-args = sys.argv[1:]
-for arg in args:
-    if arg[-3:] == '.in':
-        if 'gridconfig' in arg:
-            pass
-        elif 'config.in' in arg:
-            print(arg)
-            if isValid(arg, configInput):
-                print('MATCH')
-        elif 'geom' in arg:
-            print(arg)
-            if isValid(arg, geomInput):
-                print('MATCH')
+def main():
+    scriptPath = str(Path(__file__).parent)
+    args = sys.argv[1:]
 
+    for arg in args:
+        if arg[-3:] == '.in':
+            if 'gridconfig' in arg:
+                pass
+            elif 'config.in' in arg:
+                templateFile = scriptPath + '/template.case/config.in'
+                print()
+                print(arg)
+                if isValid(arg, templateFile):
+                    print('MATCH')
+            elif 'geom' in arg:
+                templateFile = scriptPath + '/template.case/geom01.in'
+                print()
+                print(arg)
+                if isValid(arg, templateFile):
+                    print('MATCH')
+
+if __name__ == "__main__":
+    main()
