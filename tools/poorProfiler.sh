3673ae8ad2daaeb9f38475c0c5265b9631d5380f
cibinjoseph <cibinjoseph92@gmail.com>
2021-09-20 13:47:14 +0530

    Add poor mans profielr

---
+++ tools/poorProfiler.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Poor man's profiler from http://poormansprofiler.org/
+# Outputs call stack to stdout
+nsamples=10
+sleeptime=5
+pid=$(pidof volcanor)
+
+for x in $(seq 1 $nsamples)
+  do
+    gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $pid
+    sleep $sleeptime
+  done | \
+awk '
+  BEGIN { s = ""; } 
+  /^Thread/ { print s; s = ""; } 
+  /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } 
+  END { print s }' | \
+sort | uniq -c | sort -r -n -k 1,1
