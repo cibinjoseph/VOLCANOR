e1f6db15525472c2e9f9b0ac5cd15f8472b435db
cibinjoseph <cibinjoseph92@gmail.com>
2022-03-12 22:15:12 +0530

    Add climb conQuad cases

--- tools/src_plot/plot_tip.py
+++ tools/src_plot/plot_tip.py
@@ -1,3 +1,15 @@
 OpenDatabase("Results/r01tip*.plt database")
 AddPlot("Mesh", "mesh", 1, 1)
 DrawPlots()
+
+OpenDatabase("Results/r03tip*.plt database")
+AddPlot("Mesh", "mesh", 1, 1)
+DrawPlots()
+
+OpenDatabase("Results/r05tip*.plt database")
+AddPlot("Mesh", "mesh", 1, 1)
+DrawPlots()
+
+OpenDatabase("Results/r07tip*.plt database")
+AddPlot("Mesh", "mesh", 1, 1)
+DrawPlots()
