889bdd3e50610ad9eeabbc80ef8282a42e269934
cibinjoseph <cibinjoseph92@gmail.com>
2022-03-19 16:31:10 +0530

    Correct archiving script

--- tools/archiveCase.sh
+++ tools/archiveCase.sh
@@ -3,5 +3,11 @@
 
 set -e
 
+# Compress case for archiving
+tar -I "pigz -9 -k " -cf $1.tar.gz $1
+
+# Alternative compression using zip
 # zip -9 -r $1.zip $1
-tar --use-compress-program="pigz -9 -k " $1.tar.gz $1
+
+# Uncompress archived case
+# unpigz $1
