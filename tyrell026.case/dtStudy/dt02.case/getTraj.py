abe2fbb773b38d41f2767aa5c6fe6599a05965dc
cibinjoseph <cibinjoseph92@gmail.com>
2023-01-18 01:05:44 +0530

    Add dtstudy cases

---
+++ tyrell026.case/dtStudy/dt02.case/getTraj.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# For the trajectory h = h0 + hs * sin(om*t)
+# and unit airfoil chord
+h0 = 0.5
+hs = 0.1
+k = 0.53
+vinf = 10.0
+nondimdt = 1.0/2.0
+dt = nondimdt/vinf
+ncyc = 5
+
+om = 2.0*vinf*k
+timePeriod = 2.0*np.pi/om
+totTime = ncyc * timePeriod
+
+nt = int(totTime/float(dt))
+t = np.arange(1, nt+1, 1)*dt
+w = om*hs*np.cos(om*t)
+
+for i in range(nt):
+    print("-10.0 0.0 " + str(w[i]) + " 0.0 0.0 0.0")
+
+# print("Total time = ", end="")
+# print(totTime)
+print("dt, nt = ", end="")
+print([dt, nt])
