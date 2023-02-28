6389d26cd4c408e1437ae58684277f6eb1edf58c
cibinjoseph <cibinjoseph92@gmail.com>
2023-02-28 12:38:34 +0530

    Remove seperate init_file.f90

--- src/libCommon.f90
+++ src/libCommon.f90
@@ -2,7 +2,33 @@ module libCommon
   use libMath, only : dp
   use classdef, only : switches_class, rotor_class
   implicit none
-  include "init_file.f90"
+
+  ! Contains variable declarations
+  type(rotor_class), allocatable, dimension(:) :: rotor
+
+  ! Kinematics
+  integer :: nt, nr
+  real(dp) :: dt
+
+  ! Other Variables
+  integer :: iterStart
+  real(dp) :: t
+  real(dp) :: density, velSound, kinematicVisc
+  character(len=5) :: timestamp
+  character(len=2) :: rotorChar
+  character(len=10) :: rotorFile
+  character(len=10) :: currentTime
+  logical :: fileExists
+  real(dp) :: subIterResidual
+
+  ! Iterators
+  integer :: i, is, ic, row, iter, ir, jr, ib, rowErase
+
+  ! Switches
+  type(switches_class) :: switches
+
+  ! Probes for velocity
+  real(dp), allocatable, dimension(:, :) :: probe, probeVel
 
 contains
 
